42517415b120da23904b9ce748c50c0b8a90180c
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2023-12-29 22:32:51 -0500

    Add files via upload
    This is the official working program (All files in this program are needed for the program to run properly)

--- Hackathon Project/main.js/nodemailer.js
+++ Hackathon Project/main.js/nodemailer.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const schedule = require('node-schedule');
+
 
 const app = express();
 const port = 3000;
@@ -19,22 +21,23 @@ app.get("/", async (req, res) => {
     // create client instance of auth 
     const client = await auth.getClient();
 
-    const spreadsheetId = "10qhcDyQcmq2j6LqQBefsBBB4TDIJ9Pp-3SAu81vuV1c";
+    const spreadsheetId = "1nIvS4DujCQeBvQwt1nlGYb-UnugCsxconpsC1aNguU0";
 
     // instance of google sheets api
     const googleSheets = google.sheets({ version: "v4", auth: client });
 
-    // Get metadata about spreadsheet
+    /* Get metadata about spreadsheet I learned from this method/function used in a yt video, and updated it
+     for my purposes */
     const metaData = await googleSheets.spreadsheets.get({
       auth,
       spreadsheetId,
     });
 
-    // Read emails from spreadsheet
+    // Get user emails from spreadsheet
     const getEmails = await googleSheets.spreadsheets.values.get({
       auth,
       spreadsheetId,
-      range: "Sheet1!A:A"
+      range: "form data!B:B"
     });
 
     const emails = getEmails.data.values;
@@ -43,11 +46,11 @@ app.get("/", async (req, res) => {
       return res.send('No emails found.');
     }
 
-    // Read messages from the spreadsheet
+    // Get user messages from the spreadsheet
     const getMessages = await googleSheets.spreadsheets.values.get({
       auth,
       spreadsheetId,
-      range: "Sheet1!B:B"
+      range: "form data!E:E"
     });
 
     const messages = getMessages.data.values;
@@ -55,55 +58,95 @@ app.get("/", async (req, res) => {
       console.log('No messages found in the specified range.');
       return res.send('No messages found.');
     }
+  // get user gmail subjects from spreadsheet
+    const getSubjects = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "form data!D:D"
+    });
 
-    // Ensure both emails and messages are arrays of the same length
+    const subjects = getSubjects.data.values;
+    if (!subjects) {
+      console.log('No subjects found in the specified range.');
+      return res.send('No subjects found.');
+    }
+
+    // get  gmail times from spreadsheet
+    const getDates = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "form data!C:C"
+    });
+
+    const Dates = getDates.data.values;
+    if (!subjects) {
+      console.log('No date found in the specified range.');
+      return res.send('No date found.');
+    }
+
+
+
+    // Check to see both emails and messages are arrays of the same length
     if (!Array.isArray(emails) || !Array.isArray(messages) || emails.length !== messages.length) {
       throw new Error("Invalid input. Both 'emails' and 'messages' must be arrays of the same length.");
     }
 
-    // Convert 2D arrays to 1D arrays
+    // Convert 2D arrays to 1D arrays for making it easier to read
     const formattedEmails = emails.map(entry => entry[0]);
     const formattedMessages = messages.map(entry => entry[0]);
 
-    // Send emails
-    await run(formattedEmails, formattedMessages);
+    // Sending the emails
+    await run(formattedEmails, formattedMessages, subjects, Dates);
 
-    res.send({ emails: formattedEmails, messages: formattedMessages });
+    res.send({ emails: formattedEmails, messages: formattedMessages, subjects, Dates});
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
 });
 
-async function run(emails, messages) {
-  console.log("sending emails");
+async function run(emails, messages, subjects, Dates) {
+  const scheduledDate = new Date();
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'calendarappprogram@gmail.com',
-      pass: 'xwkf yrpq achw ebqk',
-    }
-  });
+  // Loop through each email
+  for (let i = 1; i < emails.length; i++) {
+    console.log(Dates[i]);
+    const emailScheduledDate = new Date(Dates[i]);
 
-  // Loop through each recipient
-  for (let i = 0; i < emails.length; i++) {
-    try {
-      await transporter.sendMail({
-        from: '"Calendar App" <calendarapprogram@gmail.com>',
-        to: "" + emails[i] + "",
-        subject: "Subject of the email",
-        text: messages[i]
+    // Set the time to 8:30 PM for each scheduled date (for demo video)
+    emailScheduledDate.setHours(21, 44, 0, 0);
+
+
+    console.log(`Scheduling email for ${emails[i]} at ${emailScheduledDate}`);
+
+    // Schedule the email at the specified date and time
+    schedule.scheduleJob(emailScheduledDate, async function () {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'calendarappprogram@gmail.com',
+          pass: 'xwkf yrpq achw ebqk',
+        }
       });
 
-      console.log(`Email ${i + 1} sent successfully`);
-    } catch (error) {
-      console.error(`Error sending email ${i + 1}:`, error);
-    }
+      try {
+        await transporter.sendMail({
+          from: '"Calendar App" <calendarapprogram@gmail.com>',
+          to: emails[i],
+          subject: subjects[i],
+          text: messages[i]
+        });
+
+        console.log(`Email for ${emails[i]} sent successfully at ${emailScheduledDate}`);
+      } catch (error) {
+        console.error(`Error sending email for ${emails[i]} at ${emailScheduledDate}:`, error);
+      }
+    });
   }
 }
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
